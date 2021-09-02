165aa3da6ea527659fd5e45d7d2ea2ecf7091d0e
Pablo Willian Suchewschy <pblwlln@gmail.com>
2021-09-01 21:04:51 -0300

    Initial commit

---
+++ lib/httpet.ex
@@ -0,0 +1,18 @@
+defmodule HTTPet do
+  @moduledoc """
+  Documentation for `HTTPet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HTTPet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/httpet_test.exs
@@ -0,0 +1,8 @@
+defmodule HTTPetTest do
+  use ExUnit.Case
+  doctest HTTPet
+
+  test "greets the world" do
+    assert HTTPet.hello() == :world
+  end
+end
