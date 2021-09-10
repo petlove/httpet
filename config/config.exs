1322a67b10a45c6ad5ecc3761f96472ca119caf9
Pablo Willian Suchewschy <pblwlln@gmail.com>
2021-09-09 23:48:03 -0300

    Welcome Mox

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ import Config
 
 if config_env() == :test do
   config :httpet, :hosts, fake_service: "http://0.0.0.0"
+
+  # Mocks (remember keep this list as short as possible)
+
+  config :httpet, :http_client, HTTPet.Clients.ClientBehaviourMock
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,3 @@
 ExUnit.start()
+
+Mox.defmock(HTTPet.Clients.ClientBehaviourMock, for: HTTPet.Clients.ClientBehaviour)
